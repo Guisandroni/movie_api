bb44cf37c746ac0849106ccd95b16209126557e7
guisandroni <gui.sandroni@gmail.com>
2025-09-06 18:33:00 -0300

    feat: Implement endpoints for create and list category service/controller

--- src/main/java/dev/guisandroni/movieapi/Controller/CategoryController.java
+++ src/main/java/dev/guisandroni/movieapi/Controller/CategoryController.java
@@ -1,13 +1,18 @@
 package dev.guisandroni.movieapi.Controller;
 
 
+import dev.guisandroni.movieapi.Controller.Request.CategoryRequest;
+import dev.guisandroni.movieapi.Controller.Response.CategoryResponse;
 import dev.guisandroni.movieapi.Entity.Category;
+import dev.guisandroni.movieapi.Mapper.CategoryMapper;
 import dev.guisandroni.movieapi.Service.CategoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieapi/category")
@@ -17,14 +22,32 @@ public class CategoryController {
     private final CategoryService service;
     
     @GetMapping
-    public getAllCategorys(){}
+    public ResponseEntity<List<CategoryResponse>> getAllCategorys(){
+            List<CategoryResponse> categories = service.findAll()
+                    .stream()
+                    .map(category -> CategoryMapper.toCategoryResponse(category))
+                    .toList();
+            return ResponseEntity.ok(categories);
+    }
     
-    public saveCategory(){}
+    @PostMapping
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request){
+        
+        Category newCategory = CategoryMapper.toCategory(request);
+        if(newCategory!=null) {
+            Category savedCategory = service.created(newCategory);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                    .body(CategoryMapper.toCategoryResponse(savedCategory));    
+            }
+        
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        
+    }
     
-    public getCategoryById(){}
-    
-    public updateCategory(){}
-    
-    public deleteCategory(){}
+//    public getCategoryById(){}
+//    
+//    public updateCategory(){}
+//    
+//    public deleteCategory(){}
     
     }
--- src/main/java/dev/guisandroni/movieapi/Service/CategoryService.java
+++ src/main/java/dev/guisandroni/movieapi/Service/CategoryService.java
@@ -1,4 +1,26 @@
 package dev.guisandroni.movieapi.Service;
 
+
+import dev.guisandroni.movieapi.Entity.Category;
+import dev.guisandroni.movieapi.Repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+    
+    private final CategoryRepository repository;
+    
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+    
+    public Category created (Category category){
+        return repository.save(category);
+    }
+    
+    
 }
