f6880b681be1198721be42e0409d1c112aff83e4
guisandroni <gui.sandroni@gmail.com>
2025-09-22 19:24:19 -0300

    feat: custom querys using jpa repository

--- src/main/java/dev/guisandroni/movieapi/Repository/MovieRepository.java
+++ src/main/java/dev/guisandroni/movieapi/Repository/MovieRepository.java
@@ -1,9 +1,18 @@
 package dev.guisandroni.movieapi.Repository;
 
+import dev.guisandroni.movieapi.Entity.Category;
 import dev.guisandroni.movieapi.Entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    boolean existsByTitle(String title);
+    
+    List<Movie> findMovieByCategories (List<Category> categories);
+    
+    List<Movie> findTop5ByOrderByRatingDesc();
 }
