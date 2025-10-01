cc4dd463295183d2d56012cec767b2cf3370f5ee
guisandroni <gui.sandroni@gmail.com>
2025-10-01 17:28:18 -0300

    feat: config filter endpoints for user checkout authorization header

--- src/main/java/dev/guisandroni/movieapi/config/SecutiryConfig.java
+++ src/main/java/dev/guisandroni/movieapi/config/SecutiryConfig.java
@@ -1,4 +1,56 @@
 package dev.guisandroni.movieapi.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.security.sasl.AuthorizeCallback;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecutiryConfig {
+
+    private final SecutiryFilter secutiryFilter;
+    
+    //filtro de metodos post que e permitido fazer e gera authenticacao
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(AuthorizeCallback -> AuthorizeCallback
+                .requestMatchers(HttpMethod.POST, "/movie/auth/register").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/movie/auth/login").permitAll()
+                                .anyRequest().authenticated())
+                
+                .addFilterBefore(secutiryFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+                
+    }
+    
+    
+    //authentication user
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws  Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+ 
+    
+    // hash da senha/ encrypt
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
