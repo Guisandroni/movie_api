8ab1764ac6e83b50ac326f8c4c59c531c025ac4e
guisandroni <gui.sandroni@gmail.com>
2025-10-01 18:28:13 -0300

    feat: repo jpa user

--- src/main/java/dev/guisandroni/movieapi/Repository/UserRepository.java
+++ src/main/java/dev/guisandroni/movieapi/Repository/UserRepository.java
@@ -1,4 +1,20 @@
 package dev.guisandroni.movieapi.Repository;
 
-public interface UserRepositoru {
+import dev.guisandroni.movieapi.Entity.User;
+import org.apache.catalina.UserDatabase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+    Optional<UserDetails> findUserByEmail(String email);
+    
+    Optional<UserDetails> deleteByEmail(String email);
+
+    boolean existsByEmail(String email);
+
 }
