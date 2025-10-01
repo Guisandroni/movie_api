e5bab4b253ebc202359d0cc2fc62a5e0518a2e41
guisandroni <gui.sandroni@gmail.com>
2025-10-01 17:28:02 -0300

    feat: config filter endpoints for user checkout authorization header

--- src/main/java/dev/guisandroni/movieapi/config/SecutiryFilter.java
+++ src/main/java/dev/guisandroni/movieapi/config/SecutiryFilter.java
@@ -1,4 +1,37 @@
 package dev.guisandroni.movieapi.config;
 
-public class SecutiryFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class SecutiryFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+        if(Strings.isNotEmpty(authorizationHeader) && authorizationHeader.startsWith("Bearer ")){
+            String token  = authorizationHeader.substring("Bearer ".length());
+
+            
+            //config jwt
+           // Optional<JWTUserData>jwtUserData = TokenService.verifyToken(token);
+            
+            filterChain.doFilter(request, response);
+            
+            
+        } else{
+            filterChain.doFilter(request,response);
+        }
+    }
 }
