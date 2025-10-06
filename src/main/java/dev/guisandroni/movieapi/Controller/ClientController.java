40a071945b57ff6855bb4f0ac70d58a35fdd636a
guisandroni <gui.sandroni@gmail.com>
2025-10-06 11:17:15 -0300

    feat: better design email service

---
+++ src/main/java/dev/guisandroni/movieapi/Controller/ClientController.java
@@ -0,0 +1,18 @@
+package dev.guisandroni.movieapi.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
+
+public class MoviesPageController {
+    
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
